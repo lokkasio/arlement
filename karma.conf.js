4d1db3c56b6e7c4244e44f4e03ffc54740e03b1d
lokkasio <1533936+lokkasio@users.noreply.github.com>
2018-11-17 10:46:32 +0100

    add sauce labs browser testing

--- karma.conf.js
+++ karma.conf.js
@@ -1,46 +1,104 @@
 // Karma configuration
 
+const baseConfig = {
+  basePath: '',
+
+  frameworks: ['qunit'],
+
+  files: ['arlement.spec.js'],
+
+  preprocessors: {
+    'arlement.spec.js': ['rollup']
+  },
+
+  rollupPreprocessor: {
+    output: {
+      format: 'umd'
+    }
+  },
+
+  reporters: ['progress'],
+
+  autoWatch: false,
+
+  browsers: ['Firefox'],
+
+  singleRun: true
+}
+
+const customLaunchers = {
+  sl_win10_edge_14: {
+    base: 'SauceLabs',
+    browserName: 'MicrosoftEdge',
+    platform: 'Windows 10',
+    version: '14'
+  },
+  sl_win10_chrome_49: {
+    base: 'SauceLabs',
+    browserName: 'chrome',
+    platform: 'Windows 10',
+    version: '49'
+  },
+  sl_win10_firefox_51: {
+    base: 'SauceLabs',
+    browserName: 'firefox',
+    platform: 'Windows 10',
+    version: '51'
+  },
+  sl_macos1011_safari_10: {
+    base: 'SauceLabs',
+    browserName: 'safari',
+    platform: 'OS X 10.11',
+    version: '10'
+  },
+  sl_ios_safari: {
+    appiumVersion: '1.8.0',
+    base: 'SauceLabs',
+    browserName: 'Safari',
+    deviceName: 'iPhone Simulator',
+    platformVersion: '10.0',
+    platformName: 'iOS'
+  },
+  sl_android_chrome: {
+    appiumVersion: '1.9.1',
+    base: 'SauceLabs',
+    browserName: 'Chrome',
+    deviceName: 'Android GoogleAPI Emulator',
+    platformVersion: '6.0',
+    platformName: 'Android'
+  }
+}
+
+const sauceLabsConfig = {
+  reporters: ['progress', 'saucelabs'],
+
+  browsers: Object.keys(customLaunchers),
+
+  singleRun: true,
+
+  concurrency: 2,
+
+  sauceLabs: {
+    testName: 'Arlement',
+    recordScreenshots: false,
+    recordVideo: false,
+    startConnect: false,
+    build: process.env.TRAVIS_BUILD_NUMBER,
+    tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER
+  },
+
+  customLaunchers: customLaunchers
+}
+
 module.exports = function (config) {
-  config.set({
+  if (process.env.TRAVIS) {
+    if (!process.env.SAUCE_USERNAME || !process.env.SAUCE_ACCESS_KEY) {
+      console.log('Make sure the SAUCE_USERNAME and SAUCE_ACCESS_KEY environment variables are set.')
+      process.exit(1)
+    }
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
-
-    // frameworks to use
-    frameworks: ['qunit'],
-
-    // list of files / patterns to load in the browser
-    files: ['arlement.spec.js'],
-
-    // preprocess matching files before serving them to the browser
-    preprocessors: {
-      'arlement.spec.js': ['rollup']
-    },
-
-    rollupPreprocessor: {
-      output: {
-        format: 'umd'
-      }
-    },
-
-    // test results reporter to use
-    reporters: ['progress'],
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_WARN,
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
-
-    // start these browsers
-    browsers: ['Chrome', 'Firefox'],
-
-    // Continuous Integration mode
-    singleRun: true,
-
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity
-  })
+    config.set(Object.assign(baseConfig, sauceLabsConfig))
+  } else {
+    config.set(baseConfig)
+  }
 }
